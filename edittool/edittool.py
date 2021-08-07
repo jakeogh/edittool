177145c6c7640cbb3a16a71a756a73b08429ede1
jakeogh <github.com@v6y.net>
2021-08-07 11:38:36 -0700

    auto-commit

---
+++ edittool/edittool.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+# flake8: noqa           # flake8 has no per file settings :(
+# pylint: disable=C0111  # docstrings are always outdated and wrong
+# pylint: disable=C0114  #      Missing module docstring (missing-module-docstring)
+# pylint: disable=W0511  # todo is encouraged
+# pylint: disable=C0301  # line too long
+# pylint: disable=R0902  # too many instance attributes
+# pylint: disable=C0302  # too many lines in module
+# pylint: disable=C0103  # single letter var names, func name too descriptive
+# pylint: disable=R0911  # too many return statements
+# pylint: disable=R0912  # too many branches
+# pylint: disable=R0915  # too many statements
+# pylint: disable=R0913  # too many arguments
+# pylint: disable=R1702  # too many nested blocks
+# pylint: disable=R0914  # too many local variables
+# pylint: disable=R0903  # too few public methods
+# pylint: disable=E1101  # no member for base
+# pylint: disable=W0201  # attribute defined outside __init__
+# pylint: disable=R0916  # Too many boolean expressions in if statement
+# pylint: disable=C0305  # Trailing newlines editor should fix automatically, pointless warning
+# pylint: disable=C0413  # TEMP isort issue [wrong-import-position] Import "from pathlib import Path" should be placed at the top of the module [C0413]
+
+# code style:
+#   no guessing on spelling: never tmp_X always temporary_X
+#   dont_makedirs -> no_makedirs
+#   no guessing on case: local vars, functions and methods are lower case. classes are ThisClass(). Globals are THIS.
+#   del vars explicitely ASAP, assumptions are buggy
+#   rely on the compiler, code verbosity and explicitness can only be overruled by benchamrks (are really compiler bugs)
+#   no tabs. code must display the same independent of viewer
+#   no recursion, recursion is undecidiable, randomly bounded, and hard to reason about
+#   each elementis the same, no special cases for the first or last elemetnt:
+#       [1, 2, 3,] not [1, 2, 3]
+#       def this(*.
+#                a: bool,
+#                b: bool,
+#               ):
+#
+#   expicit loop control is better than while (condition):
+#       while True:
+#           # continue/break explicit logic
+
+
+import os
+import sys
+import time
+from signal import SIG_DFL
+from signal import SIGPIPE
+from signal import signal
+
+import click
+import sh
+
+signal(SIGPIPE, SIG_DFL)
+from pathlib import Path
+from typing import ByteString
+from typing import Generator
+from typing import Iterable
+from typing import List
+from typing import Optional
+from typing import Sequence
+from typing import Tuple
+from typing import Union
+
+from asserttool import eprint
+from asserttool import ic
+from asserttool import nevd
+from asserttool import not_root
+from asserttool import validate_slice
+from asserttool import verify
+#from collections import defaultdict
+#from prettyprinter import cpprint
+#from prettyprinter import install_extras
+#install_extras(['attrs'])
+#from timetool import get_timestamp
+from configtool import click_read_config
+from configtool import click_write_config_entry
+from enumerate_input import enumerate_input
+from licenseguesser import license_list
+from retry_on_exception import retry_on_exception
+#from with_sshfs import sshfs
+from with_chdir import chdir
+
+#from pathtool import path_is_block_special
+#from pathtool import write_line_to_file
+#from getdents import files
+#from prettytable import PrettyTable
+#output_table = PrettyTable()
+
+
+CFG, CONFIG_MTIME = click_read_config(click_instance=click,
+                                      app_name='newapp',
+                                      verbose=False,
+                                      debug=False,)
+
+
+# https://github.com/mitsuhiko/click/issues/441
+CONTEXT_SETTINGS = dict(default_map=CFG)
+    #dict(help_option_names=['--help'],
+    #     terminal_width=shutil.get_terminal_size((80, 20)).columns)
+
+
+#ic(CFG)
+
+
+@click.command(context_settings=CONTEXT_SETTINGS, no_args_is_help=True)
+@click.argument("paths", type=click.Path(path_type=Path), nargs=-1)
+@click.option('--apps-folder', type=str, required=True)
+@click.option('--gentoo-overlay-repo', type=str, required=True)
+@click.option('--github-user', type=str, required=True)
+@click.option('--license', type=click.Choice(license_list(verbose=False, debug=False,)), default="ISC")
+@click.option('--verbose', is_flag=True)
+@click.option('--debug', is_flag=True)
+@click.pass_context
+def cli(ctx,
+        paths: Optional[tuple[str]],
+        verbose: bool,
+        debug: bool,
+        ):
+
+    null, end, verbose, debug = nevd(ctx=ctx,
+                                     printn=False,
+                                     ipython=False,
+                                     verbose=verbose,
+                                     debug=debug,)
+
+    #global APP_NAME
+    #config, config_mtime = click_read_config(click_instance=click,
+    #                                         app_name=APP_NAME,
+    #                                         verbose=verbose,
+    #                                         debug=debug,)
+    #if verbose:
+    #    ic(config, config_mtime)
+
+    #if add:
+    #    section = "test_section"
+    #    key = "test_key"
+    #    value = "test_value"
+    #    config, config_mtime = click_write_config_entry(click_instance=click,
+    #                                                    app_name=APP_NAME,
+    #                                                    section=section,
+    #                                                    key=key,
+    #                                                    value=value,
+    #                                                    verbose=verbose,
+    #                                                    debug=debug,)
+    #    if verbose:
+    #        ic(config)
+
+    iterator = paths
+
+    index = 0
+    for index, path in enumerate_input(iterator=iterator,
+                                       dont_decode=True,  # paths are bytes
+                                       null=null,
+                                       progress=False,
+                                       skip=None,
+                                       head=None,
+                                       tail=None,
+                                       debug=debug,
+                                       verbose=verbose,):
+        path = Path(os.fsdecode(path))
+
+        if verbose:
+            ic(index, path)
+
+
