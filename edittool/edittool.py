38fcabeeacbe8efd854c90fbd3bed3c126e6dd6f
jakeogh <github.com@v6y.net>
2021-10-19 23:31:34 -0700

    auto-commit

--- edittool/edittool.py
+++ edittool/edittool.py
@@ -285,4 +285,3 @@ def edit(ctx,
                 #    sh.emerge('--tree', '--quiet-build=y', '--usepkg=n', '-1', '{group}/{short_package}'.format(group=group, short_package=short_package), _out=sys.stdout, _err=sys.stderr)
 
                 sh.sudo.emerge('--tree', '--quiet-build=y', '--usepkg=n', '-1', '{group}/{short_package}'.format(group=group, short_package=short_package), _fg=True)
-                #sh.sudo.ls("/root", _fg=True)
